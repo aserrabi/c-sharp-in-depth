1dc75e6fb4f7bf96da2b40fa90bcb53c694def1c
Serrano <ayso@gft.com>
2021-10-21 15:54:51 -0600

    Stack vs Heap

---
+++ StackVsHeap/CustomInt.cs
@@ -0,0 +1,8 @@
+﻿namespace StackVsHeap
+{
+  // internal class CustomInt // This goes to the heap, note the difference if you comment this line and uncomment next
+  internal struct CustomInt // This goes to the stack, but big structs might overflow it.
+  {
+    internal int Value { get; set; } 
+  }
+}
---
+++ StackVsHeap/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace StackVsHeap
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      BasicStackVsHeap();
+    }
+
+    private static void BasicStackVsHeap()
+    {
+      Console.WriteLine("Stacking vs Heaping");
+      
+      Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+      
+      var stackedValue = DoStack();
+      Console.WriteLine("Total Memory After Stack: {0}", GC.GetTotalMemory(false));
+      Console.WriteLine("Stacked Value {0}", stackedValue);
+      
+      var heapedValue = DoHeap();
+      Console.WriteLine("Total Memory After Heap: {0}", GC.GetTotalMemory(false));
+      Console.WriteLine("Heaped Value {0}", heapedValue);
+    }
+
+    private static int DoStack()
+    {
+      int x = 5;
+      
+      for (int i = 0; i < 1000; i++)
+      {
+        int y = x;
+        y = 4; 
+      }
+
+      return x;
+    }
+
+    private static int DoHeap()
+    {
+      var x = new CustomInt
+      {
+        Value = 5
+      };
+
+      for (int i = 0; i < 1000; i++)
+      {
+        var y = new CustomInt();
+        y = x;
+        y.Value = 4; 
+      }
+      
+      return x.Value;
+    }
+  }
+}
